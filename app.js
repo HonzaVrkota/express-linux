a78c262110d0e16809548f36a6e301130ba2d607
Honza Vrkota <vrkota.jan@hotmail.com>
2021-10-29 11:15:54 +0200

    add new route

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({
+    name: "Jan Vrkota",
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    name: "Elie",
+  });
 });
 
 app.listen(port, () => {
